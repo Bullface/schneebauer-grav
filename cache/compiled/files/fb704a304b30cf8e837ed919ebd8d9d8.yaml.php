97e9f749c3c2e860c7242b0879a6ec94775abc3a
root <josef.guet@yahoo.de>
2016-07-01 20:26:46 +0200

    initial

---
+++ cache/compiled/files/fb704a304b30cf8e837ed919ebd8d9d8.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'themes://slate/slate.yaml',
+    'modified' => 1467394406,
+    'data' => [
+        0 => 'enabled:true'
+    ]
+];
---
+++ cache/twig/1e/1ee632607dc7f3292f1719cc09a5b35f9873f5d0c88bdc4af6d925f93c071e3a.php
@@ -0,0 +1,55 @@
+<?php
+
+/* homepage.html.twig */
+class __TwigTemplate_fb0d55fc1221fcda5b608029175b9d8a138a698bd9dd81808d2d5ee124c7297d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        $this->parent = $this->loadTemplate("partials/base.html.twig", "homepage.html.twig", 1);
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "partials/base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = array())
+    {
+        // line 4
+        echo $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "content", array());
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "homepage.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,  11 => 1,);
+    }
+}
+/* {% extends 'partials/base.html.twig' %}*/
+/* */
+/* {% block content %}*/
+/* {{ page.content }}*/
+/* {% endblock %}*/
