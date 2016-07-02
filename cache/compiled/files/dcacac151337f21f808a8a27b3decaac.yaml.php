8136c0089ad908cecae1338529c35777bb3d10ab
Jneiss <nais@gmx.at>
2016-07-02 18:10:27 +0200

    fixed login

---
+++ cache/compiled/files/dcacac151337f21f808a8a27b3decaac.yaml.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/opt/lampp/htdocs/grav/user/accounts/admin.yaml',
+    'modified' => 1467475710,
+    'data' => [
+        'password' => '6FZNrXiB',
+        'email' => 'josef.guet@yahoo.de',
+        'fullname' => 'Josef Gütlinger',
+        'title' => 'Administrator',
+        'state' => 'enabled',
+        'access' => [
+            'admin' => [
+                'login' => true,
+                'super' => true
+            ],
+            'site' => [
+                'login' => true
+            ]
+        ],
+        'hashed_password' => '$2y$10$h0XbUfrNkZkrhGRXMMbDROBNPqbLFITob0eoTyeMUXZEkoT0p4Jby',
+        'reset' => '8e555f5ad3865aa892f551d48e3c5609::1468079623'
+    ]
+];
